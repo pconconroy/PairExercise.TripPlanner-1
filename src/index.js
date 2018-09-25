6e872aecff5feca4c5b3af98b21316cf72f623b8
Patrick <pdylanconroy@gmail.com>
2018-09-25 14:39:10 -0500

    map visulized

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log("Hello from JavaScript");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicGNvbmNvbnJveSIsImEiOiJjam1pNGI1bHgwMTUyM3FuemVxcnRzOTJlIn0.gmmmkQQ_RVtVxILrekR1Ug";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
