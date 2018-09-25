9a09f5b4d7320b28441448c04a153bd64862d75f
Paul Ebreo <paulebreo@triton.edu>
2018-09-25 14:52:28 -0500

    make black box

--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ mapboxgl.accessToken =
 
 const map = new mapboxgl.Map({
   container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = 'url("http://i.imgur.com/WbMOfMl.png")';
+markerDomEl.style.background = 'black';
+new mapboxgl.Marker(markerDomEl).setLngLat([-87.6354, 41.8885]).addTo(map);
